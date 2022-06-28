03c4785ba9efd1635e7090e55b5e20c8c7653478
Sommano Phivilay <sommano.phivilay@mykenzie.snhu.edu>
2022-06-28 18:47:37 -0500

    edit

--- Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,7 +1,15 @@
 package tictactoe;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         // write your code here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("---------");
+        System.out.println("| O O O |");
+        System.out.println("| O O O |");
+        System.out.println("| O O O |");
+        System.out.println("---------");
     }
 }
