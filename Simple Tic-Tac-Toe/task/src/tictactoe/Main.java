af986d11c8910239418b0d640fa66f094e542f16
Sommano Phivilay <sommano.phivilay@mykenzie.snhu.edu>
2022-07-05 16:49:30 -0500

    edit

--- Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -6,10 +6,14 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         Scanner sc = new Scanner(System.in);
+        String aaa = sc.nextLine();
+        //String aaa = "O_OXXO_XX";
+        String[] x1 = aaa.split("");
+
         System.out.println("---------");
-        System.out.println("| O O O |");
-        System.out.println("| O O O |");
-        System.out.println("| O O O |");
+        System.out.println("| " + x1[0] + " " + x1[1] + " " + x1[2] + " |");
+        System.out.println("| " + x1[3] + " " + x1[4] + " " + x1[5] + " |");
+        System.out.println("| " + x1[6] + " " + x1[7] + " " + x1[8] + " |");
         System.out.println("---------");
     }
 }
